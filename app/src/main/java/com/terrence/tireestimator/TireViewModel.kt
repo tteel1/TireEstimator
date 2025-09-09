e94dab0afa789210bf1df9b15150db056533dbe6
Terrence <teelt@ymail.com>
2025-09-08 21:51:30 -0400

    total cost logic added

--- app/src/main/java/com/terrence/tireestimator/TireViewModel.kt
+++ app/src/main/java/com/terrence/tireestimator/TireViewModel.kt
@@ -8,5 +8,20 @@ import androidx.compose.runtime.setValue
 class TireViewModel : ViewModel() {
     var pricePerTire by mutableStateOf(0.0)
     var quantity by mutableStateOf(4)
-    var taxRate by mutableStateOf(0.08625)
+
+    private val stateFee = 2.50
+    private val taxRate = 0.08625
+    private val tpmsServicePacks = 2.99
+    private val disposalFeePerTire = 2.00
+
+
+    val tireCost: Double get() = pricePerTire * quantity
+    val disposalFee: Double get() = disposalFeePerTire * quantity
+    val taxableTotal: Double get() = tireCost + tpmsServicePacks + disposalFee
+    val tax: Double get() = taxableTotal * taxRate
+    val totalCost: Double get() = taxableTotal + tax + stateFee
+
+
+
 }
+
